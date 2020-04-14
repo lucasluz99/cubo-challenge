dd6cbe25a48a5c87251381d7998b6e1cebe341d9
lucasluz99 <lucasluzthatsme@gmail.com>
2020-04-14 18:29:26 -0300

    Add footer mobile

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,11 +2,18 @@ import styled from 'styled-components';
 
 export const Container = styled.footer`
   border-top: 1px solid #828282;
+  margin-top: 20px;
 `;
 export const FooterWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 20px 0;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+
+  align-items: center;
 `;
 export const Texts = styled.div`
   color: #828282;
@@ -17,6 +24,11 @@ export const Texts = styled.div`
   }
   p {
     font-size: 15px;
+  }
+
+  @media (max-width: 800px) {
+    text-align: center;
+    margin-bottom: 20px;
   }
 `;
 export const SocialMidia = styled.nav`
@@ -29,6 +41,10 @@ export const SocialMidia = styled.nav`
     &:hover {
       opacity: 0.8;
     }
+  }
+
+  @media (max-width: 800px) {
+    width: 60%;
   }
 `;
 
